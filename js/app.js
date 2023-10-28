9e81ca37126c67a18f3662844da92a3ad8f1c1de
Jessica Hopkins <jhopkins@mutualofenumclaw.com>
2023-10-27 18:17:32 -0700

    answer to problem 3

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 
 
@@ -37,7 +37,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,11 +54,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sumAB = sum(a, b)[0]; // Calculate the sum of a and b
+  let sumABC = sum(sumAB, c)[0]; // Calculate the sum of the previous sum and c
 
+  let productAB = multiply(a, b)[0]; // Calculate the product of a and b
+  let productABC = multiply(productAB, c)[0]; // Calculate the product of the previous product and c
+
+  let sumString = `${a} and ${b} and ${c} sum to ${sumABC}.`;
+  let productString = `The product of ${a} and ${b} and ${c} is ${productABC}.`;
+
+  return [sumABC, productABC, sumString, productString];
 }
 
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
